886e51a4021ccfc8806c5a828ba2f7e4de420d56
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-01-15 23:45:52 +0500

    auth

--- inventorywebsite/settings.py
+++ inventorywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'inventorywebsite/static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
+
